719ec463c417a08e13ea4f1b8ad4ef294036d0f5
taroosg <root@taroosg.dev>
2022-12-01 10:49:48 +0900

    update files

---
+++ src/build-pages.js
@@ -0,0 +1,40 @@
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({});
+const Mustache = require('mustache');
+const fs = require("fs");
+const df = require("./date-format.js")
+
+// Build
+octokit.rest.issues.listForRepo({
+  owner: process.env.owner,
+  repo: process.env.repo,
+})
+  .then(issues => {
+
+    issues.owner = process.env.owner
+    issues.repo = process.env.repo
+
+    // Format issue object
+    issues.data.map(issue => issue.updated_at_short = df.shortDate(issue.updated_at))
+    console.log("issues: ", issues)
+
+    // Build index
+    const index_template = fs.readFileSync("templates/index.template.html", "utf8").toString();
+    const index_html = Mustache.render(index_template, issues)
+    fs.writeFileSync("index.html", index_html, "utf8");
+
+    // Build post
+    const target_issue = issues.data.filter((ti) => {
+      return ti.id == process.env.target_issue_id
+    })[0];
+
+    markdown = target_issue.body
+    const issue_template = fs.readFileSync("templates/post.template.html", "utf8").toString();
+    octokit.rest.markdown.render({ "text": markdown, "mode": "gfm" })
+      .then(issue_html => {
+        target_issue.issue_html = issue_html
+        const issue_page = Mustache.render(issue_template, target_issue)
+        fs.writeFileSync("posts/" + process.env.target_issue_id + ".html", issue_page, "utf8");
+      })
+
+  });

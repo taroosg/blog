49b0408f82fd3e118e9aacf0a44991094fb3d97f
taroosg <root@taroosg.dev>
2022-12-01 11:05:00 +0900

    fix js

--- src/build-pages.js
+++ src/build-pages.js
@@ -34,7 +34,8 @@ octokit.rest.issues.listForRepo({
       .then(issue_html => {
         target_issue.issue_html = issue_html
         const issue_page = Mustache.render(issue_template, target_issue)
-        fs.writeFileSync("../posts/" + process.env.target_issue_id + ".html", issue_page, "utf8");
+        console.log(issue_page);
+        fs.writeFileSync("posts/" + process.env.target_issue_id + ".html", issue_page, "utf8");
       })
 
   });
